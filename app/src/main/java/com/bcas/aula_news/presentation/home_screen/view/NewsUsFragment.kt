8e6d434d93f4be31d575a3bb55eea3c6e960891f
aulafaridatuz <aulafaridatuz@gmail.com>
2024-03-28 09:11:10 +0700

    add fragment news id,us,de

---
+++ app/src/main/java/com/bcas/aula_news/presentation/home_screen/view/NewsUsFragment.kt
@@ -0,0 +1,19 @@
+package com.bcas.aula_news.presentation.home_screen.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.bcas.aula_news.base.BaseFragment
+import com.bcas.aula_news.databinding.FragmentNewsUsBinding
+
+class NewsUsFragment: BaseFragment<FragmentNewsUsBinding>() {
+    override fun inflateBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentNewsUsBinding {
+        return FragmentNewsUsBinding.inflate(inflater,container, false)
+    }
+
+    override fun setUpView() {
+    }
+
+}
