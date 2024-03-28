3e026ee8db471c2680c6830272277d339af058c3
aulafaridatuz <aulafaridatuz@gmail.com>
2024-03-28 09:09:55 +0700

    add HomeTabAdapter.kt

--- app/src/main/java/com/bcas/aula_news/presentation/detail_screen/adapter/HomeTabAdapter.kt
+++ app/src/main/java/com/bcas/aula_news/presentation/detail_screen/adapter/HomeTabAdapter.kt
@@ -1,4 +1,27 @@
 package com.bcas.aula_news.presentation.detail_screen.adapter
 
-class HomeTabAdapter {
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class HomeTabAdapter(
+    fragment: Fragment,
+) : FragmentStateAdapter(
+    fragment.childFragmentManager,
+    fragment.lifecycle
+) {
+    private val fragmentContent = arrayListOf<Fragment>()
+
+    fun addListFrament(fragment: Fragment) {
+        fragmentContent.add(fragment)
+    }
+
+    override fun getItemCount(): Int {
+        return fragmentContent.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentContent[position]
+    }
+
+
 }
